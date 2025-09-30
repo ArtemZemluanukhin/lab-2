bc1a31fd0535136e7568e26aaa83d61bbf7f6794
ArtemZemluanukhin <zemluanukhin.07@gmail.com>
2025-09-30 16:20:36 +0300

    Add files via upload

---
+++ авиакомпания.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int P2,S1;
+	float res;
+	setlocale(LC_CTYPE, "RUS");
+	puts("Введите количество пассажиров\n");
+	scanf("%d",&P2);
+	printf("На борту %d пассажиров\n", P2);
+	puts("Введите стоимость билета 2 класса\n");
+	scanf("%d", &S1);
+	res = P2 * 0.75 * S1 + P2 * 0.25 * S1;
+	printf("Общая сумма, которую получила авиакомпания-%.2f\n", res);
+	system("pause");
+
+}
